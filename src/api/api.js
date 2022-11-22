9db906d62d9b9bafcdad1a53f0df20a412ea6730
unknown <afwan.rizky96@gmail.com>
2022-11-22 13:29:25 +0700

    add backend address

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
   // baseURL: 'https://young-mountain-05032.herokuapp.com/',
+  baseURL: 'https://stark-garden-87613.herokuapp.com/',
 });
